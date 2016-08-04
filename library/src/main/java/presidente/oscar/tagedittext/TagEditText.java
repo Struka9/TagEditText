356a6c37339eb1fb2e0995b5d0b1ee02850741bd
Oscar Rene Flores Presidente <oscar.rene1989@gmail.com>
2016-08-04 14:00:20 -0600

    Changed the way we split the tags

--- library/src/main/java/presidente/oscar/tagedittext/TagEditText.java
+++ library/src/main/java/presidente/oscar/tagedittext/TagEditText.java
@@ -6,22 +6,49 @@ import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.text.Editable;
+import android.text.InputFilter;
 import android.text.Spannable;
 import android.text.SpannableStringBuilder;
+import android.text.Spanned;
 import android.text.TextWatcher;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
 import android.text.style.ImageSpan;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
-import android.widget.MultiAutoCompleteTextView;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by oscarr on 8/2/16.
  */
-public class TagEditText extends MultiAutoCompleteTextView {
+public class TagEditText extends EditText {
 
+    //TODO: Make sure we only add the token if is not in the list
     private LayoutInflater mLayoutInflater;
+
+    private boolean mShouldTriggerTextWatcher = true;
+
+    private InputFilter mFilter = new InputFilter() {
+        @Override
+        public CharSequence filter(CharSequence source, int start, int end, Spanned dest, int dstart, int dend) {
+
+            if (source != null && source.toString().contains(",")) {
+                return "";
+            }
+
+            return null;
+        }
+    };
+
+    private List<Tag> mTagList;
+
 
     public TagEditText(Context context) {
         this(context, null);
@@ -37,14 +64,19 @@ public class TagEditText extends MultiAutoCompleteTextView {
     }
 
     private void init() {
+        setMovementMethod(LinkMovementMethod.getInstance());
+        mTagList = new ArrayList<>();
+        setFilters(new InputFilter[]{mFilter});
         mLayoutInflater = LayoutInflater.from(getContext());
+
 
         //Whenever the user presses "Enter" we add the text
         setOnEditorActionListener(new OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_UP) {
-                    TagEditText.this.append(",");
+                    checkForNewTag();
+                    setTags();
                 }
                 return true;
             }
@@ -53,65 +85,167 @@ public class TagEditText extends MultiAutoCompleteTextView {
         addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                if (after < count && mShouldTriggerTextWatcher) {
+                    //The user is deleting tags with the inputmethod
+                    removeByPositionInText(start, count);
+                }
             }
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if (count >= 1) {
-                    if (s.charAt(start) == ',') {
-                        setTags();
-                    }
-                }
             }
 
             @Override
             public void afterTextChanged(Editable s) {
             }
         });
+    }
 
-        setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
+    @Override
+    protected void onSelectionChanged(int selStart, int selEnd) {
+
+        CharSequence text = getText();
+        if (text != null) {
+            if (selStart != text.length() || selEnd != text.length()) {
+                setSelection(text.length(), text.length());
+                return;
+            }
+        }
+        //super.onSelectionChanged(selStart, selEnd);
+    }
+
+    private void removeByTagValue(String tagValue) {
+        int found = -1;
+        int count = -1;
+
+        for (int i = 0; i < mTagList.size(); i++) {
+            Tag tag = mTagList.get(i);
+            if (found != -1) {
+//                Tag prev = mTagList.get(i - 1);
+//                Tag next = i == mTagList.size() - 1 ? null : mTagList.get(i + 1);
+
+//                tag.from = prev.to;
+//                tag.to = next != null ? next.from : getText().length();
+                tag.from -= count;
+                tag.to -= count;
+            }
+
+            if (tag.tokenValue.compareTo(tagValue) == 0) {
+                found = i;
+                count = tag.to - tag.from;
+            }
+
+        }
+
+        if (found != -1) {
+            Tag removed = mTagList.remove(found);
+
+            String textAsString = getText().toString();
+            String firstSlice = textAsString.substring(0, removed.from);
+            String secondSlice = textAsString.substring(removed.to, textAsString.length());
+
+            mShouldTriggerTextWatcher = false;
+            setText(firstSlice + secondSlice);
+            mShouldTriggerTextWatcher = true;
+            setTags();
+        }
+    }
+
+    private void removeByPositionInText(int start, int count) {
+
+        //It can actually be many tags at the same time
+        int found = -1;
+        for (int i = 0; i < mTagList.size(); i++) {
+            Tag tag = mTagList.get(i);
+
+            if (tag.from == start) {
+                found = i;
+                break;
+            }
+
+            if (found != -1) {
+                tag.from -= count;
+                tag.to -= count;
+            }
+        }
+
+        if (found != -1) {
+            mTagList.remove(found);
+        }
+    }
+
+    private void checkForNewTag() {
+        String wholeText = getText().toString();
+        if (mTagList.size() > 0) {
+            Tag lastTag = mTagList.get(mTagList.size() - 1);
+            String newTagValue = wholeText.substring(lastTag.to);
+
+            if (newTagValue.length() > 0) {
+                Tag newTag = new Tag();
+                newTag.from = lastTag.to;
+                newTag.to = wholeText.length();
+                newTag.tokenValue = newTagValue;
+
+                mTagList.add(newTag);
+            }
+            setTags();
+        } else if (wholeText.length() > 0) {
+            Tag newTag = new Tag();
+            newTag.from = 0;
+            newTag.to = wholeText.length();
+            newTag.tokenValue = wholeText;
+
+            mTagList.add(newTag);
+            setTags();
+        }
     }
 
     private void setTags() {
         Editable editable = getText();
+        SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(editable);
 
-        if (editable.toString().contains(",")) {
-            SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder(editable);
+        for (int i = 0; i < mTagList.size(); i++) {
+            final Tag tag = mTagList.get(i);
+            TextView tv = (TextView) mLayoutInflater.inflate(R.layout.tag_layout, null, false);
+            tv.setText(tag.tokenValue);
 
-            String[] tags = editable.toString().split(",");
+            GradientDrawable d = (GradientDrawable) tv.getBackground();
 
-            int x = 0;
-            for (String tag : tags) {
-                TextView tv = (TextView) mLayoutInflater.inflate(R.layout.tag_layout, null, false);
-                tv.setText(tag);
+            int measureSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
+            tv.measure(measureSpec, measureSpec);
+            tv.layout(0, 0, tv.getMeasuredWidth(), tv.getMeasuredHeight());
 
-                GradientDrawable d = (GradientDrawable) tv.getBackground();
+            Bitmap b = Bitmap.createBitmap(tv.getWidth(), tv.getHeight(), Bitmap.Config.ARGB_8888);
+            Canvas canvas = new Canvas(b);
+            canvas.translate(-tv.getScrollX(), -tv.getScrollY());
+            tv.draw(canvas);
+            tv.setDrawingCacheEnabled(true);
 
-                int measureSpec = MeasureSpec.makeMeasureSpec(0, MeasureSpec.UNSPECIFIED);
-                tv.measure(measureSpec, measureSpec);
-                tv.layout(0, 0, tv.getMeasuredWidth(), tv.getMeasuredHeight());
+            Bitmap cacheBm = tv.getDrawingCache();
+            Bitmap bitmap = cacheBm.copy(Bitmap.Config.ARGB_8888, true);
+            tv.destroyDrawingCache();
 
-                Bitmap b = Bitmap.createBitmap(tv.getWidth(), tv.getHeight(), Bitmap.Config.ARGB_8888);
-                Canvas canvas = new Canvas(b);
-                canvas.translate(-tv.getScrollX(), -tv.getScrollY());
-                tv.draw(canvas);
-                tv.setDrawingCacheEnabled(true);
+            BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
+            bitmapDrawable.setBounds(0, 0, bitmapDrawable.getIntrinsicWidth(), bitmapDrawable.getIntrinsicHeight());
 
-                Bitmap cacheBm = tv.getDrawingCache();
-                Bitmap bitmap = cacheBm.copy(Bitmap.Config.ARGB_8888, true);
-                tv.destroyDrawingCache();
-
-                BitmapDrawable bitmapDrawable = new BitmapDrawable(getResources(), bitmap);
-                bitmapDrawable.setBounds(0, 0, bitmapDrawable.getIntrinsicWidth(), bitmapDrawable.getIntrinsicHeight());
-
-                spannableStringBuilder.setSpan(new ImageSpan(bitmapDrawable), x, x + tag.length(),  Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
-
-                x += tag.length() + 1;
-
-            }
-
-            setText(spannableStringBuilder);
-            setSelection(getText().length());
+            spannableStringBuilder.setSpan(new ImageSpan(bitmapDrawable), tag.from, tag.to, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+            spannableStringBuilder.setSpan(new ClickableSpan() {
+                @Override
+                public void onClick(View widget) {
+                    //removeByTagValue(tag.tokenValue);
+                    //setSelection(getText().length());
+                }
+            }, tag.from, tag.to, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
         }
+
+        setText(spannableStringBuilder);
+        setSelection(getText().length());
+    }
+
+    private class Tag {
+        String tokenValue;
+        //The indices in the text that represent the range of this tag
+        int from;
+        int to;
     }
 }
